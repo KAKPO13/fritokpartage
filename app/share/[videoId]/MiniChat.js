aae82eb7cc2ffbb0edac3ce633907b1896dde8de
KAKPO COFFI GABRIEL <fritok013@gmailcom>
2025-08-08 17:00:14 +0000

    Switch to SSR build for Netlify

--- app/share/[videoId]/MiniChat.js
+++ app/share/[videoId]/MiniChat.js
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState, useRef } from 'react';
 import { collection, addDoc, query, where, onSnapshot, getFirestore, serverTimestamp } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
