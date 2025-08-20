9f40b196c872868e9ccbac624361463bb89d151b
KAKPO COFFI GABRIEL <fritok013@gmailcom>
2025-08-20 08:22:47 +0000

    Switch to SSR build for Netlify

--- app/capture/AddCommandeForm.js
+++ app/capture/AddCommandeForm.js
@@ -9,7 +9,7 @@ import { createClient } from '@supabase/supabase-js';
 import 'react-toastify/dist/ReactToastify.css';
 
 // Supabase config
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl = process.env.supabaseUrl;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
