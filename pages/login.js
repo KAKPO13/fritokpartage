8129230e462b95a16d4151d56b0c3243b86dba2c
KAKPO COFFI GABRIEL <fritok013@gmailcom>
2026-04-22 23:28:53 +0000

    refactor(auth):

--- pages/login.js
+++ pages/login.js
@@ -13,6 +13,7 @@ import {
   sendPasswordResetEmail, onAuthStateChanged,
 } from "firebase/auth";
 
+
 const firebaseConfig = {
   apiKey           : process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain       : process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
